b5bc478e43681f0891dc566d2b9abd46daa6720a
jinlilac24 <jinlilac24@gmail.com>
2025-01-27 12:57:28 +0900

    🚀 [Feat]: Constants 생성

---
+++ src/constants/about.ts
@@ -0,0 +1,66 @@
+export const PROFILE = [
+  {
+    title: "ABOUT",
+    content: [
+      { label: "Name", text: "김수현" },
+      { label: "Birth", text: "1999.02.19" },
+      { label: "Email", text: "jinlilac24@gmail.com" },
+    ],
+  },
+  {
+    title: "EDUCATION",
+    content: [
+      {
+        label: "2023.10 ~ 2024.03",
+        text: "이젠 아카데미 UI/UX 웹 퍼블리셔 프론트엔드 양성과정 우수상 수료",
+      },
+      {
+        label: "2018.08 ~ 2022.06",
+        text: "북경과학기술대학교 국제무역학 졸업",
+      },
+    ],
+  },
+  {
+    title: "CERTIFICATE",
+    content: [
+      { label: "2020.09", text: "운전면허 2종" },
+      { label: "2022.05", text: "중어 HSK 6급" },
+      { label: "2023.07", text: "TOEIC-Speaking Lv.IH" },
+    ],
+  },
+];
+
+export const SKILL = [
+  {
+    label: "JavaScript",
+    tip: "이벤트 핸들링을 통해 사용자의 동작에 반응하고, DOM 을 조작하여 콘텐츠를 동적으로 변경할 수 있습니다.",
+  },
+  {
+    label: "TypeScript",
+    tip: "변수나 함수의 타입을 명시적으로 정의할 수 있으며, 타입 추론을 통해 코드를 더 안전하게 만들 수 있습니다.",
+  },
+  {
+    label: "React",
+    tip: "Atomic design 패턴을 활용하여 재사용한 요소를 만들 수 있으며, hook과 Recoil 로 상태 관리와 효율적인 방향으로 구현 할 수 있습니다.",
+  },
+  {
+    label: "HTML",
+    tip: "유효성 문제 없이 HTML5 웹 접근성에 올바르게 구조를 구성할 수 있습니다.",
+  },
+  {
+    label: "CSS",
+    tip: "CSS 미디어쿼리를 활용하여 다양한 디바이스에 적합한 레이아웃을 최적화하고, 디자인 및 애니메이션을 구성할 수 있습니다.",
+  },
+  {
+    label: "TailwindCSS-Dark",
+    tip: "빠른 개발 속도, 일관된 디자인을 재사용하여 효율적인 UI를 구현할 수 있습니다.",
+  },
+  {
+    label: "Figma-Dark",
+    tip: " 레이아웃, 그룹핑, 컴포넌트, 스타일링 등의 다양한 기능을 활용하여 UI/UX 디자인을 구성할 수 있습니다.",
+  },
+  {
+    label: "Github-Dark",
+    tip: "pullRequest로 버전 및 이슈 관리를 통해 버그 수정과 기능 개선을 체계적으로 진행할 수 있습니다.",
+  },
+];
