a4a2e99a468b840538bdc6a078f1c9ec5fb62499
jinlilac24 <jinlilac24@gmail.com>
2025-08-05 14:57:30 +0900

    ♻️ [Refactor]: project 리스트, 상세보기 패칭 함수로 분리

--- src/hooks/useProject.ts
+++ src/hooks/useProject.ts
@@ -1,12 +1,39 @@
+// src/hooks/useProject.ts
 import { supabase } from "@/supabaseClient";
 import { ProjectType } from "@/types/project";
 import { queryOptions } from "@tanstack/react-query";
 
-export const ProjectQuery = () =>
+// 전체 프로젝트 목록 가져오는 옵션
+export const ProjectListQuery = () =>
   queryOptions<ProjectType[]>({
-    queryKey: ["project"],
+    queryKey: ["projects"],
     queryFn: async () => {
-      const { data } = await supabase.from("project").select("*");
-      return data as ProjectType[];
+      const { data, error } = await supabase
+        .from("project")
+        .select("*")
+        .order("order", { ascending: true })
+        .eq("show", true);
+      if (error) throw error;
+      return data!;
     },
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
+  });
+
+// 단일 프로젝트 가져오는 옵션
+export const ProjectDetailQuery = (id: number) =>
+  queryOptions<ProjectType>({
+    queryKey: ["project", id],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("project")
+        .select("*")
+        .eq("id", id)
+        .single();
+      if (error) throw error;
+      return data!;
+    },
+    enabled: id > 0,
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
   });
